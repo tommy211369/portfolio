f1a3aab82c69c887bf05cf6d185105da4b82fe0e
tommy211369 <tommy.thongrasamy@gmail.com>
2021-09-20 16:13:31 +0200

    add projects and images

---
+++ src/assets/projects.js
@@ -0,0 +1,25 @@
+import gamepad from "./img/gamepad.jpg";
+import vinted from "./img/vinted.jpg";
+import marvel from "./img/marvel.jpg";
+import deliveroo from "./img/deliveroo.jpg";
+
+const list = [
+  {
+    title: "Gamepad",
+    img: gamepad,
+  },
+  {
+    title: "Réplique de Vinted",
+    img: vinted,
+  },
+  {
+    title: "Marvel",
+    img: marvel,
+  },
+  {
+    title: "Réplique de Deliveroo",
+    img: deliveroo,
+  },
+];
+
+export default list;
--- src/containers/Projects.js
+++ src/containers/Projects.js
@@ -1,8 +1,20 @@
+import { Link } from "react-router-dom";
+import list from "../assets/projects";
+
 const Projects = () => {
   return (
     <div className="projects pages">
-      <h1>Projets</h1>
       {/* Affichage des différents projets */}
+      <div className="projects-list">
+        {list.map((item, index) => {
+          return (
+            <Link className="project">
+              <img src={item.img} alt={item.title} />
+              <h2 key={index}>{item.title}</h2>
+            </Link>
+          );
+        })}
+      </div>
     </div>
   );
 };
