6e6d215fc06630357ac44dedddd627f1b9710f97
tommy211369 <tommy.thongrasamy@gmail.com>
2021-10-20 16:33:54 +0200

    add meteo project and responsive page

--- src/assets/next.js
+++ src/assets/next.js
@@ -1,6 +1,7 @@
 import happyCowMobile from "./img/happy-cow-mobile.jpg";
 import yukaMobile from "./img/yuka-mobile.jpg";
 import vintedMobile from "./img/vinted-mobile.jpg";
+import meteoApp from "./img/meteoApp.jpg";
 
 const next = [
   {
@@ -17,6 +18,11 @@ const next = [
     title: "Réplique de Happy Cow (Mobile App)",
     img: happyCowMobile,
     description: "Une réplique simplifiée de l'application mobile de Happy Cow",
+  },
+  {
+    title: "Meteo App (Web App)",
+    img: meteoApp,
+    description: "Une application météo utilisant l'API d'OpenWeatherMap.",
   },
 ];
 
