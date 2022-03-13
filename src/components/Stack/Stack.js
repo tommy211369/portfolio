62da5a774b9efa037457af2d2aef36d6b7f05bc9
tommy211369 <tommy.thongrasamy@gmail.com>
2022-03-13 16:32:55 +0100

    update stacks

--- src/components/Stack/Stack.js
+++ src/components/Stack/Stack.js
@@ -6,12 +6,12 @@ import react from "../../assets/img/react.png";
 import redux from "../../assets/img/redux.png";
 // import next from "../../assets/img/next.png";
 // import graphql from "../../assets/img/graphql.png";
-import nodejs from "../../assets/img/nodejs.png";
-import express from "../../assets/img/express1.png";
-import mongodb from "../../assets/img/mongodb.png";
+// import nodejs from "../../assets/img/nodejs.png";
+// import express from "../../assets/img/express1.png";
+// import mongodb from "../../assets/img/mongodb.png";
 import firebase from "../../assets/img/firebase.png";
 import netlify from "../../assets/img/netlify.png";
-import heroku from "../../assets/img/heroku.png";
+// import heroku from "../../assets/img/heroku.png";
 import git from "../../assets/img/git.png";
 import Fade from "react-reveal/Fade";
 import "./Stack.css";
@@ -32,12 +32,12 @@ function Stack() {
           <Logo techno={redux} stack="Redux" />
           {/* <Logo techno={next} stack="" /> */}
           {/* <Logo techno={graphql} stack="GraphQL" /> */}
-          <Logo techno={nodejs} stack="Node.js" />
-          <Logo techno={express} stack="" />
-          <Logo techno={mongodb} stack="MongoDB" />
+          {/*<Logo techno={nodejs} stack="Node.js" />*/}
+          {/*<Logo techno={express} stack="" />*/}
+          {/*<Logo techno={mongodb} stack="MongoDB" />*/}
           <Logo techno={firebase} stack="Firebase" />
           <Logo techno={netlify} stack="" />
-          <Logo techno={heroku} stack="" />
+          {/*<Logo techno={heroku} stack="" />*/}
           <Logo techno={git} stack="Git" />
         </div>
       </Fade>
